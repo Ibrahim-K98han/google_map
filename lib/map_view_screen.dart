e82fd8cffc021adaf9945f6e56568802efc2c1c4
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-04-25 21:37:26 +0600

    google map added

--- lib/map_view_screen.dart
+++ lib/map_view_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapViewScreen extends StatefulWidget {
@@ -9,13 +10,39 @@ class MapViewScreen extends StatefulWidget {
 }
 
 class _MapViewScreenState extends State<MapViewScreen> {
+  Position? currentLocation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Map View'),),
-      body: GoogleMap(
-
+      appBar: AppBar(
+        title: Text('Map View'),
       ),
+      body: Center(
+        child: Text(currentLocation.toString()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          //location permission
+          LocationPermission permission = await Geolocator.requestPermission();
+          if (permission == LocationPermission.always ||
+              permission == LocationPermission.whileInUse) {
+            //current location fetch
+            currentLocation = await Geolocator.getCurrentPosition();
+            print(currentLocation);
+            setState(() {});
+          }else{
+            print('Permission not allowed');
+          }
+        },
+        child: Icon(Icons.location_on),
+      ),
+      // body:
+      // GoogleMap(
+      //   initialCameraPosition: CameraPosition(
+      //     target: LatLng(23.793895737073488, 90.40448580672911)
+      //   ),
+      // ),
     );
   }
 }

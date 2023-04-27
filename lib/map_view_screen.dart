afc14a11ab72284363805c150d17203859e475d4
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-04-27 09:15:04 +0600

    google map matkers,circle,polygons added

--- lib/map_view_screen.dart
+++ lib/map_view_screen.dart
@@ -10,7 +10,6 @@ class MapViewScreen extends StatefulWidget {
 }
 
 class _MapViewScreenState extends State<MapViewScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +17,71 @@ class _MapViewScreenState extends State<MapViewScreen> {
         title: const Text('Map View'),
       ),
       body: GoogleMap(
+        zoomControlsEnabled: false,
+        onTap: (LatLng position) {
+          print(position);
+        },
+        mapType: MapType.normal,
+        //marker add
+        markers: {
+          Marker(
+            markerId: MarkerId('bubt'),
+            position: LatLng(23.811972774158328, 90.35684009419076),
+            infoWindow: InfoWindow(title: 'Bubt'),
+          ),
+          Marker(
+              markerId: MarkerId('Sheraton office'),
+              position: LatLng(23.811543083199535, 90.3544619679451),
+              infoWindow: InfoWindow(title: 'Sheraton office'),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueBlue)),
+          Marker(
+              markerId: MarkerId('draggable'),
+              position: LatLng(23.81109463312196, 90.35585269331933),
+              infoWindow: InfoWindow(title: 'Draggable'),
+              draggable: true,
+              onDrag: (LatLng initLatLng) {},
+              onDragEnd: (LatLng onDragEnd) {},
+              onDragStart: (LatLng onDragStart) {})
+        },
+        //circle add
+        circles: {
+          Circle(
+              circleId: CircleId('office-circle'),
+              center: LatLng(23.811972774158328, 90.35684009419076),
+              radius: 50,
+              fillColor: Colors.pink.shade100,
+              strokeWidth: 2,
+              strokeColor: Colors.pink),
+          Circle(
+              circleId: CircleId('sheraton-circle'),
+              center: LatLng(23.811543083199535, 90.3544619679451),
+              radius: 50,
+              fillColor: Colors.pink.shade100,
+              strokeWidth: 2,
+              strokeColor: Colors.pink)
+        },
+        //polygon add
+        polygons: {
+          Polygon(
+            polygonId: PolygonId('abc'),
+            visible: true,
+            fillColor: Colors.teal.shade100,
+            strokeWidth: 2,
+            strokeColor: Colors.teal,
+            points: [
+              LatLng(23.8102428190993, 90.355999879539),
+              LatLng(23.81352244112977, 90.35701576620339),
+              LatLng(23.813421832931468, 90.35828981548548)
+
+            ]
+          )
+        },
         initialCameraPosition: CameraPosition(
-          target: LatLng(23.81248451251643, 90.35984537003183),
-          zoom: 18,
-        ),
+            target: LatLng(23.811972774158328, 90.35684009419076),
+            zoom: 17,
+            bearing: 40,
+            tilt: 0),
       ),
     );
   }
